a33860f1505a6715a0ce9a89a09827b1c9a24b25
Ali Stoica Kovacs <alistoicakovacs@icloud.com>
2022-04-09 14:03:44 +0200

    fix pages not opening

--- script.js
+++ script.js
@@ -6,61 +6,20 @@ const secondButton = document.querySelector(".button-second");
 const thirdButton = document.querySelector(".button-third");
 
 // First Page
-firstButton.addEventListener("click", function () {
-	clickEvent(firstButton);
-});
-secondButton.addEventListener("click", function () {
-	clickEvent(secondButton);
-});
-thirdButton.addEventListener("click", function () {
-	clickEvent(thirdButton);
-});
+firstButton.addEventListener("click", clickEvent);
 
-// Second Page
+secondButton.addEventListener("click", clickEvent);
 
-// secondButton.addEventListener("click", function () {
-// 	secondPageDiv.classList.add("active");
+thirdButton.addEventListener("click", clickEvent);
 
-// 	if (secondButton.clicked == true || secondButton.innerText === "Close") {
-// 		secondButton.innerText = "Second Page";
-// 		secondPageDiv.classList.remove("active");
-// 	} else if (
-// 		secondButton.clicked == true ||
-// 		secondButton.innerText == "Second Page"
-// 	) {
-// 		secondButton.innerText = "Close";
-// 	}
-// });
-
-const clickEvent = function (button) {
+function clickEvent() {
 	if (document.querySelector(".active") !== null) {
 		document.querySelector(".active").classList.remove("active");
 	}
-	if (button == firstButton) firstPageDiv.classList.add("active");
-	if (button == secondButton) secondPageDiv.classList.add("active");
-	if (button == thirdButton) thirdPageDiv.classList.add("active");
-};
-// if (button.clicked == true || button.innerText === "Close") {
-// 	button.innerText = "First Page";
-// 	div.classList.remove("active");
-// } else if (button.clicked == true || button.innerText == "First Page") {
-// 	button.innerText = "Close";
-// }
-// Third Page
-
-// firstButton.addEventListener("click", function () {
-// 	firstPageDiv.classList.add("active");
-
-// 	if (firstButton.clicked == true || firstButton.innerText === "Close") {
-// 		firstButton.innerText = "First Page";
-// 		firstPageDiv.classList.remove("active");
-// 	} else if (
-// 		firstButton.clicked == true ||
-// 		firstButton.innerText == "First Page"
-// 	) {
-// 		firstButton.innerText = "Close";
-// 	}
-// });
+	if (this == firstButton) firstPageDiv.classList.add("active");
+	if (this == secondButton) secondPageDiv.classList.add("active");
+	if (this == thirdButton) thirdPageDiv.classList.add("active");
+}
 
 /* 
 1. Check if the page has the active class => with query selector
